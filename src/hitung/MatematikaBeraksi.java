bfeca7bd66fad0d65763bae4034af93a3cfcb410
Fath-rhm <fathrahman00@gmail.com>
2025-06-10 09:17:01 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author ASUS
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika fathur = new Matematika(6,3);
+        
+        System.out.println("Hasil Penjumlahan: "+fathur.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+fathur.setPengurangan());
+        System.out.println("Hasil Perkalian: "+fathur.setPerkalian());
+        System.out.println("Hasil Pembagian: "+fathur.setPembagian());
+    }
 }
